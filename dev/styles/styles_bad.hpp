0e1208ecd662b73d3d043497241a93baee47e903
LEPESME 'Jiboo' Jean-Bapiste <lepesme.jb@gmail.com>
2011-05-30 20:18:56 +0200

    Added license header

--- dev/styles/styles_bad.hpp
+++ dev/styles/styles_bad.hpp
@@ -1,3 +1,8 @@
+/*
+Mugu by LEPESME "Jiboo" Jean-Baptiste is licensed under a Creative Commons Attribution 3.0 Unported License
+http://creativecommons.org/licenses/by/3.0/
+*/
+
 #include <string>
 #include <cairo>
 
