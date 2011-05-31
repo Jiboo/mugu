5e3709f2c190ec930e298aea9bc7c9038fdf524d
LEPESME 'Jiboo' Jean-Bapiste <lepesme.jb@gmail.com>
2011-05-31 19:49:43 +0200

    Reworked on anim design

--- dev/anims/anims.cpp
+++ dev/anims/anims.cpp
@@ -4,41 +4,17 @@ http://creativecommons.org/licenses/by/3.0/
 */
 
 #include "anims.hpp"
-#include <xcb/xcb.h>
 
-void set_dialog_width(xcb_connection_t *connection, xcb_window_t window, uint32_t width)
-{
-	xcb_configure_window(connection, window, XCB_CONFIG_WINDOW_WIDTH, &width);
-	xcb_flush(connection);
-}
+std::vector<std::thread*> context::garbages;
 
 int main(int,char**)
 {
-	xcb_connection_t *connection   = xcb_connect(NULL, NULL);
-	const xcb_setup_t *setup       = xcb_get_setup(connection);
-	xcb_screen_iterator_t iter     = xcb_setup_roots_iterator(setup);
-	xcb_screen_t *screen           = iter.data;
-	xcb_window_t window            = xcb_generate_id(connection);
+	context ctx;
+
+	dialog test(ctx.connection);
 	
-	xcb_create_window(connection, XCB_COPY_FROM_PARENT, window, screen->root, 0, 0, 150, 150, 10, XCB_WINDOW_CLASS_INPUT_OUTPUT, screen->root_visual, 0, NULL );
-    xcb_map_window(connection, window);
-    xcb_flush(connection);
-
-	uint32_t values = 300;
-	xcb_configure_window(connection, window, XCB_CONFIG_WINDOW_WIDTH, &values);
-	xcb_configure_window(connection, window, XCB_CONFIG_WINDOW_HEIGHT, &values);
-	
-	std::thread* th = anim(values, 800, std::chrono::seconds(3), &transitions::ease, [&values, connection, window]
-	{
-		xcb_configure_window(connection, window, XCB_CONFIG_WINDOW_WIDTH, &values);
-		xcb_flush(connection);
-	});
-	th->join();
-	delete th;
-
-	th = anim(std::bind(set_dialog_width, connection, window, std::placeholders::_1), 800, 300, std::chrono::seconds(3), &transitions::ease);
-	th->join();
-	delete th;
-
-	xcb_disconnect(connection);
+	anim(&test, &dialog::set_width, 500, 200, std::chrono::seconds(3));
+	std::this_thread::sleep_for(std::chrono::seconds(3));
+	anim(&test, &dialog::set_width, 200, 500, std::chrono::seconds(3));
 }
+
--- dev/anims/anims.hpp
+++ dev/anims/anims.hpp
@@ -7,16 +7,90 @@ http://creativecommons.org/licenses/by/3.0/
 #define _GLIBCXX_USE_NANOSLEEP
 
 #include <cmath>
+#include <vector>
 #include <thread>
 #include <iostream>
 #include <functional>
 
+#include <xcb/xcb.h>
+
 // https://github.com/madrobby/emile
+
+class context
+{
+public:
+	static std::vector<std::thread*> garbages;
+	xcb_connection_t *connection;
+	
+	context()
+	{
+		connection = xcb_connect(NULL, NULL);
+	}
+	
+	~context()
+	{
+		for(std::thread* garbage : garbages)
+		{
+			if(garbage->joinable())
+				garbage->join();
+			delete garbage;
+		}
+		xcb_disconnect(connection);
+	}
+	
+	static void recycle(std::thread* pGarbage)
+	{
+		garbages.push_back(pGarbage);
+	}
+};
+
+class widget
+{
+public:
+	unsigned width;
+
+	virtual void set_width(unsigned pWidth)
+	{
+		this->width = pWidth;
+	}
+	
+	unsigned get_width()
+	{
+		return this->width;
+	}
+};
+
+class dialog : public widget
+{
+public:
+	xcb_connection_t *connection;
+	xcb_window_t window;
+
+	dialog(xcb_connection_t *pConnection)
+		: window(xcb_generate_id(connection)), connection(pConnection)
+	{
+		this->width = 500;
+		
+		const xcb_setup_t *setup = xcb_get_setup(connection);
+		xcb_screen_iterator_t iter = xcb_setup_roots_iterator(setup);
+		xcb_screen_t *screen = iter.data;
+	
+		xcb_create_window(connection, XCB_COPY_FROM_PARENT, window, screen->root, 0, 0, this->width, 150, 10, XCB_WINDOW_CLASS_INPUT_OUTPUT, screen->root_visual, 0, NULL);
+		xcb_map_window(connection, window);
+		xcb_flush(connection);
+	}
+	
+	virtual void set_width(unsigned pWidth)
+	{
+		widget::set_width(pWidth);
+		xcb_configure_window(connection, window, XCB_CONFIG_WINDOW_WIDTH, &width);
+		xcb_flush(connection);
+	}
+};
 
 namespace transitions
 {
-
-	double none(double pPos)
+	double linear(double pPos)
 	{
 		return pPos;
 	}
@@ -26,54 +100,15 @@ namespace transitions
 		return (-cos(pPos * M_PI) / 2) + 0.5;
 	}
 }
-	
-template <typename tDataType, typename tPassedDataType, class Rep, class Period>
-std::thread * anim(
-	tDataType &pData, 
-	tPassedDataType pTarget, 
-	std::chrono::duration<Rep, Period> pDuration, 
-	std::function<double (double)> pTransition = &transitions::none,
-	std::function<void (void)> pStep = []{},
-	std::function<void (void)> pEnd = []{})
-{
-	return new std::thread([&pData, pTarget, pDuration, pTransition, pStep, pEnd]
-	{
-		tDataType origin = pData;
-		
-		auto start = std::chrono::system_clock::now();
-		auto time = std::chrono::system_clock::now();
-		
-		std::chrono::milliseconds duration = std::chrono::duration_cast<std::chrono::milliseconds>(pDuration);
-		
-		while(time < (start + duration))
-		{
-			std::chrono::milliseconds diff = std::chrono::duration_cast<std::chrono::milliseconds>(time - start);		
-			pData = origin + (pTarget - origin) * pTransition((double)diff.count() / (double)duration.count());
-			
-			pStep();
 
-			time = std::chrono::system_clock::now();
-			//std::this_thread::yield(); // Would be nice if no pStep callback
-			std::this_thread::sleep_for(std::chrono::milliseconds(20));
-		}
-		
-		pData = pTarget;
-		
-		pEnd();
-	});
-}
-
-template <typename tFuncType, typename tPassedDataType, class Rep, class Period>
-std::thread * anim(
-	tFuncType pFunc,
-	tPassedDataType pOrigin,
-	tPassedDataType pTarget,
+template <typename tWidgetType, typename tDataType, typename tPassedType, class Rep, class Period>
+void anim(
+	tWidgetType* pWidget, void(tWidgetType::*pFunc)(tDataType),
+	tPassedType pOrigin, tPassedType pTarget,
 	std::chrono::duration<Rep, Period> pDuration,
-	std::function<double (double)> pTransition = &transitions::none,
-	std::function<void (void)> pStep = []{},
-	std::function<void (void)> pEnd = []{})
+	std::function<double (double)> pTransition = &transitions::ease)
 {
-	return new std::thread([pFunc, pOrigin, pTarget, pDuration, pTransition, pStep, pEnd]
+	context::recycle(new std::thread([pWidget, pFunc, pOrigin, pTarget, pDuration, pTransition]
 	{
 		auto start = std::chrono::system_clock::now();
 		auto time = std::chrono::system_clock::now();
@@ -83,18 +118,13 @@ std::thread * anim(
 		while(time < (start + duration))
 		{
 			std::chrono::milliseconds diff = std::chrono::duration_cast<std::chrono::milliseconds>(time - start);		
-			pFunc(pOrigin + (pTarget - pOrigin) * pTransition((double)diff.count() / (double)duration.count()));
+			((pWidget)->*(pFunc))(pOrigin + (pTarget - pOrigin) * pTransition((double)diff.count() / (double)duration.count()));
 			
-			pStep();
-
 			time = std::chrono::system_clock::now();
-			//std::this_thread::yield(); // Would be nice if no pStep callback
 			std::this_thread::sleep_for(std::chrono::milliseconds(20));
 		}
 		
-		pFunc(pTarget);
-		
-		pEnd();
-	});
+		((pWidget)->*(pFunc))(pTarget);
+	}));
 }
 
