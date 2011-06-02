c4e26b0058328e477ffab53667d2d0fc75156864
LEPESME 'Jiboo' Jean-Bapiste <lepesme.jb@gmail.com>
2011-06-02 17:34:29 +0200

    Improvements to styles

--- inc/mugu/styles.hpp
+++ inc/mugu/styles.hpp
@@ -59,18 +59,18 @@ enum font_decoration
 	pType pName##_right; \
 	pType pName##_bottom; \
 	pType pName##_left; \
-	inline void set_##pName(pType pSize) \
+	void set_##pName(pType pSize) \
 	{ pName##_top = pSize; pName##_right = pSize; pName##_bottom = pSize; pName##_left = pSize; } \
-	inline void set_##pName(pType pSizeTB, pType pSizeRL) \
+	void set_##pName(pType pSizeTB, pType pSizeRL) \
 	{ pName##_top = pSizeTB; pName##_right = pSizeRL; pName##_bottom = pSizeTB; pName##_left = pSizeRL; } \
-	inline void set_##pName(pType pSizeT, pType pSizeRL, pType pSizeB) \
+	void set_##pName(pType pSizeT, pType pSizeRL, pType pSizeB) \
 	{ pName##_top = pSizeT; pName##_right = pSizeRL; pName##_bottom = pSizeB; pName##_left = pSizeRL; } \
-	inline void set_##pName(pType pSizeT, pType pSizeR, pType pSizeB, pType pSizeL) \
+	void set_##pName(pType pSizeT, pType pSizeR, pType pSizeB, pType pSizeL) \
 	{ pName##_top = pSizeT; pName##_right = pSizeR; pName##_bottom = pSizeB; pName##_left = pSizeL; }
 	
 #define MUGU_STYLE_SOURCE(pName) \
-	source* pName##_source; \
-	void set_##pName##_rgba(double pR, double pG, double pB, double pA = 1) \
+	protected: source* pName##_source; \
+	public: void set_##pName##_rgba(double pR, double pG, double pB, double pA = 1) \
 	{ delete this->pName##_source; this->pName##_source = new source_rgba(pR, pG, pB, pA); } \
 	void set_##pName##_png(const char* pPath) \
 	{ delete this->pName##_source; this->pName##_source = new source_png(pPath); } \
@@ -81,7 +81,28 @@ enum font_decoration
 	MUGU_STYLE_SOURCE(pName##_top); \
 	MUGU_STYLE_SOURCE(pName##_right); \
 	MUGU_STYLE_SOURCE(pName##_bottom); \
-	MUGU_STYLE_SOURCE(pName##_left);
+	MUGU_STYLE_SOURCE(pName##_left); \
+	public:void set_##pName##_rgba(double pR, double pG, double pB, double pA = 1) \
+	{ \
+		delete this->pName##_top_source; this->pName##_top_source = new source_rgba(pR, pG, pB, pA); \
+		delete this->pName##_right_source; this->pName##_right_source = new source_rgba(pR, pG, pB, pA); \
+		delete this->pName##_bottom_source; this->pName##_bottom_source = new source_rgba(pR, pG, pB, pA); \
+		delete this->pName##_left_source; this->pName##_left_source = new source_rgba(pR, pG, pB, pA); \
+	} \
+	void set_##pName##_png(const char* pPath) \
+	{ \
+		delete this->pName##_top_source; this->pName##_top_source = new source_png(pPath); \
+		delete this->pName##_right_source; this->pName##_right_source = new source_png(pPath); \
+		delete this->pName##_bottom_source; this->pName##_bottom_source = new source_png(pPath); \
+		delete this->pName##_left_source; this->pName##_left_source = new source_png(pPath); \
+	} \
+	void set_##pName##_gradient(double pX0, double pY0, double pX1, double pY1, std::initializer_list<gradient_color_stop> pStops) \
+	{ \
+		delete this->pName##_top_source; this->pName##_top_source = new source_gradient(pX0, pY0, pX1, pY1, pStops); \
+		delete this->pName##_right_source; this->pName##_right_source = new source_gradient(pX0, pY0, pX1, pY1, pStops); \
+		delete this->pName##_bottom_source; this->pName##_bottom_source = new source_gradient(pX0, pY0, pX1, pY1, pStops); \
+		delete this->pName##_left_source; this->pName##_left_source = new source_gradient(pX0, pY0, pX1, pY1, pStops); \
+	}
 
 #define MUGU_STYLE_TEXT(pName) \
 	double pName##_size; \
